554acdb2cb316ea33bbb940b9ef3011095b6ef97
Ikey Benzaken <ikey.benz@gmail.com>
2018-08-31 12:52:25 -0700

    Bootstrap requires jquery. So i added jquery.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let methodOverride = require('method-override');
 let app = express();
 let reviewController = require('./controllers/reviews');
 let port = process.env.PORT || 5000;
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
