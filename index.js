6dab8dbe3adb6041d35c63d3b63277b05ce8e247
Ikey Benzaken <ikey.benz@gmail.com>
2018-08-31 12:49:30 -0700

    Forgot to change the port from 5000 to process.env.PORT...

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let bodyParser = require('body-parser');
 let methodOverride = require('method-override');
 let app = express();
 let reviewController = require('./controllers/reviews');
-
+let port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -12,4 +12,4 @@ app.set('view engine', 'handlebars');
 
 reviewController(app);
 
-app.listen(5000, console.log("Running Rotten Potatoes on port 5000!"));
+app.listen(port, console.log(`Running Rotten Potatoes on port ${port}!`));
